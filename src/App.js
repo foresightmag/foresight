692433e837cee6bb693ac0530095d3636c62fed5
Panha-Sim <psim@my.gcu.edu>
2023-02-20 22:13:49 -0700

    Install boostrap, react-boostrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant='dark'>test</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,9 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
+      
     </div>
   );
 }
