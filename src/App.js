017de940d3bbf2eac6c90939fa5ca3562217396e
Panha-Sim <psim@my.gcu.edu>
2023-02-22 12:24:30 -0700

    Card Class
    Create card and card container. I disable the view of the scroll bar, still need to work on the scrolling horizontally.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Card(){
+ return (
+  <div className='Card' href='/'>
+    <div className='CardInside'></div>
+  </div>
+ );
+}
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant='dark'>test</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
-      
+      <h1>Get to know the team</h1>
+     <div className='CardContainer MAIN'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </div>
     </div>
   );
 }
