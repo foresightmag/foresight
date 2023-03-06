f89fe6aaff87f545dfbf9b9569940825f682f90b
Nathan Dilla <nathandilla@Nathans-MacBook-Pro.local>
2023-03-05 17:56:48 -0700

    front page update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,19 @@
 import '../css/Nav.css';
 import React from 'react';
-import logo from "../images/BLACK-FORESIGHT-LOGO.png";
+import logo from "../images/BLUE-FORESIGHT-LOGO.svg";
 export default function()
 {
     return (
-        <div className='Nav' href='/'>
-          <nav>
-            <a href=""><img className="nav-logo" src={logo} height="10px"/></a>
-            <a href="">Global</a>
-            <a href="">Student Interview</a>
-            <a href="">Prof. Interview</a>
-            <div className="issue-title">Issue 01 Women in STEM</div>
-          </nav>
+        <div className="Nav" href='/'>
+          <div className="nav-container">
+            <nav className="nav">
+              <a href=""><img className="nav-logo" src={logo} height="10px"/></a>
+              <a href="">Global</a>
+              <a href="">Student Interview</a>
+              <a href="">Prof. Interview</a>
+              <div className="issue-title">Issue 01 Women in STEM</div>
+            </nav>
+          </div>
         </div>
        );
 }
